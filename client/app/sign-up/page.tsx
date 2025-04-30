ae52028965ff36992ff7feec09a37cff28ce7d3a
ayPromise servant <fatherpother@gmail.com>
2025-04-30 23:20:28 +0300

    <frontend>: feat: scaffold /sign-up page

---
+++ client/app/sign-up/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const dynamic = 'force-dynamic';
+
+
+const SignUpPage: React.FC = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold mb-4">SignUpPage</h1>
+            <p className="text-lg">
+                Ми є провідною школою підготовки операторів БПЛА, з фокусом на сучасні технології та бойову ефективність.
+            </p>
+        </>
+    );
+}
+
+export default SignUpPage
