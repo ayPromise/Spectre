ae9db5337a16488dd60ae628bba3499553d5b9b6
ayPromise servant <fatherpother@gmail.com>
2025-04-30 23:23:27 +0300

    <frontend>: feat: scaffold /profile/certificate page

---
+++ client/app/profile/certificate/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const dynamic = 'force-dynamic';
+
+
+const CertificatesPage: React.FC = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold mb-4">Certificates</h1>
+            <p className="text-lg">
+                Ми є провідною школою підготовки операторів БПЛА, з фокусом на сучасні технології та бойову ефективність.
+            </p>
+        </>
+    );
+}
+
+export default CertificatesPage
