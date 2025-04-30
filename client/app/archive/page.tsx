ca914d306731ab2302ffa17dccd8f8177419946d
ayPromise servant <fatherpother@gmail.com>
2025-04-30 23:24:50 +0300

    <frontend>: feat: scaffold /archive page

---
+++ client/app/archive/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const dynamic = 'force-dynamic';
+
+
+const ArchivePage: React.FC = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold mb-4">Archive</h1>
+            <p className="text-lg">
+                Ми є провідною школою підготовки операторів БПЛА, з фокусом на сучасні технології та бойову ефективність.
+            </p>
+        </>
+    );
+}
+
+export default ArchivePage
