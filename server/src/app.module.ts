8b0e0f3b16c89bcdebc0abab4179f107c3b6e6c2
ayPromise servant <fatherpother@gmail.com>
2025-05-03 00:11:59 +0300

    <backend>: feat: added UserResolver to the Nest.js module configuration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UserResolver } from './graphql/resolvers/UserResolver';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // default apollo playground is going to be deprecated
+      graphiql: true,
+
+      // where to generate schemes
+      autoSchemaFile: 'src/scheme.gql',
+      sortSchema: true,
+    }),
+  ],
+  providers: [UserResolver],
+  controllers: [],
 })
 export class AppModule {}
