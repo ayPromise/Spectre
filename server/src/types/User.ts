64c3194d73660e0eb03283ce5d6be74e48659e63
ayPromise servant <fatherpother@gmail.com>
2025-05-03 00:09:18 +0300

    <backend>: feat: created ts interface for User data + mocks

---
+++ server/src/types/User.ts
@@ -0,0 +1,26 @@
+export type UserRole = 'student' | 'instructor' | 'admin';
+
+export interface IUser {
+  // confidential info
+  id: string;
+  email?: string;
+  hashedPassword: string;
+
+  // personal info
+  firstName: string;
+  lastName: string;
+  phone: string;
+  role: UserRole;
+  profileImageUrl?: string;
+
+  // user's activity
+  createdAt: Date;
+  updatedAt: Date;
+  lastLogin?: Date | null; // can be absent for the first login
+
+  // user's progress
+  completedArticles: string[]; // their ids
+  completedLectures: string[]; // their ids
+  completedVideos: string[]; // their ids
+  certificates: string[]; // their ids
+}
