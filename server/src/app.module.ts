918e091b408985c184206c819237ea1e19ae2f12
ayPromise servant <fatherpother@gmail.com>
2025-05-03 11:08:04 +0300

    <backend>: feat: added JwtModule for auth purposes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,16 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UserResolver } from './graphql/resolvers/UserResolver';
 import { AuthResolver } from './graphql/resolvers/AuthResolver';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    // to use .env vars globally
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       // default apollo playground is going to be deprecated
@@ -14,6 +21,18 @@ import { AuthResolver } from './graphql/resolvers/AuthResolver';
       // where to generate schemes
       autoSchemaFile: 'src/scheme.gql',
       sortSchema: true,
+    }),
+
+    // enabling jwt
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN'),
+        },
+      }),
     }),
   ],
   providers: [UserResolver, AuthResolver],
