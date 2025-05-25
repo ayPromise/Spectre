d71ae456c904ebbe2266813c70166115bbda6646
Danylo Dziubchuk <fatherpother@gmail.com>
2025-05-25 18:10:56 +0300

    <client>: feat: used apply form component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,34 +1,40 @@
-import { Metadata } from 'next';
-import { JSX } from 'react';
+import { Metadata } from "next";
+import { JSX } from "react";
+import ApplySection from "./components/ApplySection";
 
 export const metadata: Metadata = {
-  title: 'Welcome - Icarus Eye',
-  description: 'Learn about our UAV operator training school.',
+  title: "Welcome - Icarus Eye",
+  description: "Learn about our UAV operator training school.",
 };
 
 export default function AboutPage(): JSX.Element {
   return (
     <>
-      <h1 className="text-3xl font-bold mb-4" id="about">Про нас</h1>
+      <h1 className="text-3xl font-bold mb-4" id="about">
+        Про нас
+      </h1>
       <p className="text-lg">
-        Ми є провідною школою підготовки операторів БПЛА, з фокусом на сучасні технології та бойову ефективність.
+        Ми є провідною школою підготовки операторів БПЛА, з фокусом на сучасні
+        технології та бойову ефективність.
       </p>
 
-      <h1 className="text-3xl font-bold mb-4" id="faq">FAQ</h1>
+      <h1 className="text-3xl font-bold mb-4" id="faq">
+        FAQ
+      </h1>
       <p className="text-lg">
-        1. Ми є провідною школою підготовки операторів БПЛА, з фокусом на сучасні технології та бойову ефективність.
+        1. Ми є провідною школою підготовки операторів БПЛА, з фокусом на
+        сучасні технології та бойову ефективність.
       </p>
       <p className="text-lg">
-        2. Ми є провідною школою підготовки операторів БПЛА, з фокусом на сучасні технології та бойову ефективність.
+        2. Ми є провідною школою підготовки операторів БПЛА, з фокусом на
+        сучасні технології та бойову ефективність.
       </p>
       <p className="text-lg">
-        3. Ми є провідною школою підготовки операторів БПЛА, з фокусом на сучасні технології та бойову ефективність.
+        3. Ми є провідною школою підготовки операторів БПЛА, з фокусом на
+        сучасні технології та бойову ефективність.
       </p>
 
-      <h1 className="text-3xl font-bold mb-4" id='apply'>Apply</h1>
-      <p className="text-lg">
-        Ми є провідною школою підготовки операторів БПЛА, з фокусом на сучасні технології та бойову ефективність.
-      </p>
+      <ApplySection />
     </>
   );
 }
