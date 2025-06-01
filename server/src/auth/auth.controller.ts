b5321c5b646b3bfe53501506c6eb7df68c59499d
Danylo Dziubchuk <fatherpother@gmail.com>
2025-06-01 20:13:40 +0300

    <server>: feat: on successful auth remember token

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { Response } from "express";
 
 @Controller('auth')
 export class AuthController {
@@ -12,10 +13,17 @@ export class AuthController {
   }
 
   @Post('sign-in')
-  async signIn(
-    @Body('email') email: string,
-    @Body('password') password: string
-  ) {
-    return this.authService.signIn(email, password);
+  async signIn(@Res({ passthrough: true }) res: Response, @Body() body: { email: string; password: string }) {
+
+    const token = await this.authService.signIn(body.email, body.password);
+
+    res.cookie('token', token.access_token, {
+      httpOnly: true,
+      secure: false, // 👉 true на проді з HTTPS
+      sameSite: 'lax',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+  
+    return { message: 'Ласкаво просимо!' };
   }
 }
