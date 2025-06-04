3a3b00db97405aa300c02855bc7cf4c8b65defcd
Danylo Dziubchuk <fatherpother@gmail.com>
2025-06-04 23:57:05 +0300

    <client>: chore: toast appears on bottom right corner

--- client/utils/toast.ts
+++ client/utils/toast.ts
@@ -2,12 +2,14 @@ import { toast } from 'react-toastify';
 
 export const showSuccess = (message: string) =>
   toast.success(message, {
-    position: 'top-right',
+    position: 'bottom-right',
     autoClose: 3000,
+    className:"border border-black"
   });
 
 export const showError = (message: string) =>
   toast.error(message, {
-    position: 'top-right',
+    position: 'bottom-right',
     autoClose: 3000,
+    className:"border border-black"
   });
