03c00ee364aae2049c8e250a73b48f41b9abda86
Danylo Dziubchuk <fatherpother@gmail.com>
2025-06-16 00:19:08 +0300

    <client>: fix: wrapped /schedules into auth protection

--- client/app/schedule/layout.tsx
+++ client/app/schedule/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import AuthLayout from "../layouts/AuthLayout";
 
 export const metadata: Metadata = {
   title: "Розклад | SPECTRE",
@@ -10,5 +11,5 @@ export default function ScheduleLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  return <AuthLayout>{children}</AuthLayout>;
 }
